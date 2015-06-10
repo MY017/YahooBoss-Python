9cf285846f8b865e37a221d9a40eba01b2476764
Jeremiah Malina <jmalina327@gmail.com>
2015-06-10 16:37:53 -0400

    yahooboss/__init__.py: added error handling in search web

--- yahooboss/__init__.py
+++ yahooboss/__init__.py
@@ -6,151 +6,158 @@ import json
 from traceback import format_exc
 import requests
 
+
+class APIError(Exception):
+    pass
+
+
 class GeoCodeError(Exception):
-	pass
+    pass
 
 
 class YahooBoss(object):
 
-	def __init__(self, key, secret,**kwargs):
-		self.key = key
-		self.secret = secret
+    def __init__(self, key, secret,**kwargs):
+        self.key = key
+        self.secret = secret
+
 
 class BossSearch(YahooBoss):
 
-	def __init__(self, key, secret, **kwargs):
-		super(BossSearch, self).__init__(key,secret,**kwargs)
+    def __init__(self, key, secret, **kwargs):
+        super(BossSearch, self).__init__(key,secret,**kwargs)
 
-		self.params = {
-			'oauth_version': "1.0",
-			'format': 'json'
-		}
+        self.params = {
+            'oauth_version': "1.0",
+            'format': 'json'
+        }
 
-		self._set_params(**kwargs)
+        self._set_params(**kwargs)
 
-	
-	def _set_params( self, **kwargs ):
-		self.params['count'] = kwargs.get('results_per_page',50)
-		self.age = kwargs.get('age', '7d')
-		self.age = kwargs.get('age','7d')
-		self.urls = kwargs.get('urls',[])
+    def _set_params( self, **kwargs ):
+        self.params['count'] = kwargs.get('results_per_page',50)
+        self.age = kwargs.get('age', '7d')
+        self.age = kwargs.get('age','7d')
+        self.urls = kwargs.get('urls',[])
 
-		if self.urls:
-			param_str = ")OR(".join(self.urls)
-			self.params['url'] = "(" + param_str + ")"
-		
-		# looks like Yahoo is going away from urls and uses "sites" now
-		if kwargs.get('sites'):
-			sites = kwargs.get('sites')
+        if self.urls:
+            param_str = ")OR(".join(self.urls)
+            self.params['url'] = "(" + param_str + ")"
 
-			if isinstance(sites, list ):
-				self.params['sites'] = ','.join(sites)
-			else:
-				self.params['sites'] = sites
+        # looks like Yahoo is going away from urls and uses "sites" now
+        if kwargs.get('sites'):
+            sites = kwargs.get('sites')
 
-	def search_news(self,q,page_num=1,**kwargs):
-		
-		self._set_params(**kwargs)
+            if isinstance(sites, list ):
+                self.params['sites'] = ','.join(sites)
+            else:
+                self.params['sites'] = sites
 
-		start = (page_num - 1) * self.params.get('count')
+    def search_news(self, q, page_num=1,**kwargs):
 
-		return self.make_request('news',q, start)
+        self._set_params(**kwargs)
 
+        start = (page_num - 1) * self.params.get('count')
 
-	def search_web(self, q, page_num=1,**kwargs ):
-		self._set_params(**kwargs)
+        return self.make_request('news',q, start)
 
-		start = (page_num - 1) * self.params.get('count')
-		return self.make_request('web',q,start)
+    def search_web(self, q, page_num=1,**kwargs ):
+        self._set_params(**kwargs)
 
+        start = (page_num - 1) * self.params.get('count')
+        return self.make_request('web',q,start)
 
-	def make_request(self,bucket, q,start_at):
-		url = "http://yboss.yahooapis.com/ysearch/"+bucket
-		consumer = oauth.Consumer(key=self.key,secret=self.secret)
-	
-		try:
-			new_params = {
-				'oauth_nonce': oauth.generate_nonce(),
-				'oauth_timestamp': int(time.time()),
-				'q' : urllib.quote_plus(q),
-				'start': start_at
-			}
-		except:
-			# weird unicode problems sometimes
-			return []
+    def make_request(self,bucket, q,start_at):
+        url = "http://yboss.yahooapis.com/ysearch/"+bucket
+        consumer = oauth.Consumer(key=self.key,secret=self.secret)
 
-		r_params = dict( self.params.items() + new_params.items() )
-		if bucket == 'news':
-			r_params['age'] = self.age
+        try:
+            new_params = {
+                'oauth_nonce': oauth.generate_nonce(),
+                'oauth_timestamp': int(time.time()),
+                'q' : urllib.quote_plus(q),
+                'start': start_at
+            }
+        except:
+            # weird unicode problems sometimes
+            return []
 
-		req = oauth.Request(method="GET",url=url,parameters=r_params)
-		signature_method = oauth.SignatureMethod_HMAC_SHA1()
+        r_params = dict( self.params.items() + new_params.items() )
+        if bucket == 'news':
+            r_params['age'] = self.age
 
-		req.sign_request(signature_method, consumer, None)
-		
+        req = oauth.Request(method="GET",url=url,parameters=r_params)
+        signature_method = oauth.SignatureMethod_HMAC_SHA1()
 
-		#f = urllib.urlopen(req.to_url())
-		f = requests.get(req.to_url())
+        req.sign_request(signature_method, consumer, None)
+        f = requests.get(req.to_url())
 
-		retval = f.json()
-		self.total_results = retval.get('bossresponse').get(bucket).get('totalresults')
+        retval = f.json()
 
-		if self.total_results:
-			results = retval.get('bossresponse').get(bucket).get('results')
-		else:
-			results = []
+        if 'error' in retval:
+            raise APIError(retval['error']['description'])
 
-		return results
+        self.total_results = retval.get(
+            'bossresponse', {}).get(bucket, {}).get('totalresults', {})
+
+        if self.total_results:
+            results = retval.get(
+                'bossresponse', {}).get(bucket, {}).get(
+                'results',)
+        else:
+            results = []
+
+        return results
 
 
 class PlaceFinder(YahooBoss):
-	# placefinder geocoding
+    # placefinder geocoding
 
-	def __init__(self, key,secret, **kwargs):
-		super(PlaceFinder,self).__init__(key,secret,**kwargs)
-		self.params = {}
+    def __init__(self, key,secret, **kwargs):
+        super(PlaceFinder,self).__init__(key,secret,**kwargs)
+        self.params = {}
 
-	def placefinder(self, q):
-		self.params['q'] = urllib.quote_plus(q)
-		return self.make_request()
+    def placefinder(self, q):
+        self.params['q'] = urllib.quote_plus(q)
+        return self.make_request()
 
-	def lookup(self,q):
-		return self.placefinder(q)
+    def lookup(self,q):
+        return self.placefinder(q)
 
-	def reverse(self, lat, lon ):
-		self.params['gflags'] = 'R'
-		self.params['location'] = urllib.quote_plus(str(lat) + ' ' + str(lon))
-		return self.make_request()
-	
-	def make_request(self):
-		params = {	
-			'oauth_version': "1.0",
-			'oauth_nonce': oauth.generate_nonce(),
-			'oauth_timestamp': int(time.time()),
-			'count': '1',
-			'flags': 'J'
-		}
+    def reverse(self, lat, lon ):
+        self.params['gflags'] = 'R'
+        self.params['location'] = urllib.quote_plus(str(lat) + ' ' + str(lon))
+        return self.make_request()
 
-		params.update( self.params )
+    def make_request(self):
+        params = {
+            'oauth_version': "1.0",
+            'oauth_nonce': oauth.generate_nonce(),
+            'oauth_timestamp': int(time.time()),
+            'count': '1',
+            'flags': 'J'
+        }
 
-		bucket = 'placefinder'
-		url =  "http://yboss.yahooapis.com/geo/" + bucket
+        params.update( self.params )
 
-		consumer = oauth.Consumer(key=self.key,secret=self.secret)
+        bucket = 'placefinder'
+        url =  "http://yboss.yahooapis.com/geo/" + bucket
 
-		req = oauth.Request(method="GET", url=url, parameters=params)
-	
-		signature_method = oauth.SignatureMethod_HMAC_SHA1()
+        consumer = oauth.Consumer(key=self.key,secret=self.secret)
 
-		req.sign_request(signature_method, consumer, None)
-		
-		retval = requests.get(req.to_url())
-		
-		response = retval.json()
+        req = oauth.Request(method="GET", url=url, parameters=params)
 
-		if response and response.get('bossresponse'):
-			return response.get('bossresponse').get('placefinder')
-		else:
-			raise GeoCodeError( retval )
+        signature_method = oauth.SignatureMethod_HMAC_SHA1()
+
+        req.sign_request(signature_method, consumer, None)
+
+        retval = requests.get(req.to_url())
+
+        response = retval.json()
+
+        if response and response.get('bossresponse'):
+            return response.get('bossresponse').get('placefinder')
+        else:
+            raise GeoCodeError( retval )
 
 
